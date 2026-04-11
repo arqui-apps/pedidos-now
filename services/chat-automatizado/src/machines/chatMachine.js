1ce400c6d9c8abf2dbdf9c3f80ba682552635783
MoyseSac <moises.sacm@gmail.com>
2026-04-11 17:17:00 -0600

    fix: chatMachine know expects real user information id_user node src/tests/testChatMachineInput.mjs

--- services/chat-automatizado/src/machines/chatMachine.js
+++ services/chat-automatizado/src/machines/chatMachine.js
@@ -1,1 +1,48 @@
-z
+import { createMachine } from "xstate";
+import { clienteStates } from "./states/clienteStates.js";
+import { repartidorStates } from "./states/repartidorStates.js";
+import { negocioStates } from "./states/negocioStates.js";
+
+export const chatMachine = createMachine({
+    id: "chatbot",
+    initial: "INICIO",
+    context: ({ input }) => ({
+        id_usuario: input?.id_usuario ?? null,
+        user_type: input?.user_type ?? null,
+        order_code: input?.order_code ?? null,
+        order_data: input?.order_data ?? null,
+        compensation: input?.compensation ?? null,
+        error: input?.error ?? null,
+    }),
+    states: {
+        INICIO: {
+            always: [
+                {
+                    guard: ({ context }) => context.user_type === "cliente",
+                    target: "MENU_PRINCIPAL_CLIENTE",
+                },
+                {
+                    guard: ({ context }) =>
+                        context.user_type === "repartidor",
+                    target: "MENU_PRINCIPAL_REPARTIDOR",
+                },
+                {
+                    guard: ({ context }) => context.user_type === "negocio",
+                    target: "MENU_PRINCIPAL_NEGOCIO",
+                },
+            ],
+        },
+
+        ESCALAR_AGENTE: {
+            type: "final",
+        },
+
+        RESUELTO: {
+            type: "final",
+        },
+
+        ...clienteStates,
+        ...repartidorStates,
+        ...negocioStates,
+    },
+});
