a047fa6a9a2d97d8439b1c0944b2293fc9f0231b
MoyseSac <moises.sacm@gmail.com>
2026-04-16 18:05:59 -0600

    feat(models): register EscalationPayload model and add association to ChatSession

--- services/chat-automatizado/src/models/index.js
+++ services/chat-automatizado/src/models/index.js
@@ -8,6 +8,7 @@ import OptionChoiceModel from "./OptionChoice.js";
 import CompensationModel from "./Compensation.js";
 import SupportRequestModel from "./SupportRequest.js";
 import FaqModel from "./Faq.js";
+import EscalationPayloadModel from "./EscalationPayload.js";
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -27,6 +28,7 @@ const OptionChoice = OptionChoiceModel(sequelize, DataTypes);
 const Compensation = CompensationModel(sequelize, DataTypes);
 const SupportRequest = SupportRequestModel(sequelize, DataTypes);
 const Faq = FaqModel(sequelize, DataTypes);
+const EscalationPayload = EscalationPayloadModel(sequelize, DataTypes);
 
 ChatSession.hasMany(Mensaje, { foreignKey: "id_session" });
 Mensaje.belongsTo(ChatSession, { foreignKey: "id_session" });
@@ -36,6 +38,9 @@ Compensation.belongsTo(ChatSession, { foreignKey: "id_session" });
 
 ChatSession.hasMany(SupportRequest, { foreignKey: "id_session" });
 SupportRequest.belongsTo(ChatSession, { foreignKey: "id_session" });
+
+ChatSession.hasMany(EscalationPayload, { foreignKey: "id_session" });
+EscalationPayload.belongsTo(ChatSession, { foreignKey: "id_session" });
 
 Menu.hasMany(OptionChoice, { foreignKey: "id_menu" });
 OptionChoice.belongsTo(Menu, { foreignKey: "id_menu" });
@@ -49,4 +54,5 @@ export {
     Compensation,
     SupportRequest,
     Faq,
+    EscalationPayload,
 };
