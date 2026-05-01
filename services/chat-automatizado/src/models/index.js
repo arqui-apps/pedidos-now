0ad97a2a0f60bd5f3a2dcb47e120526b71ee4599
MoyseSac <moises.sacm@gmail.com>
2026-05-01 13:32:32 -0600

    fix: use private network for sequelize runtime connection

--- services/chat-automatizado/src/models/index.js
+++ services/chat-automatizado/src/models/index.js
@@ -14,7 +14,8 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST || "localhost",
+        host: process.env.DB_PRIVATE_HOST || process.env.DB_HOST,
+        port: process.env.DB_PRIVATE_PORT || process.env.DB_PORT,
         dialect: "mysql",
         logging: false,
     }
