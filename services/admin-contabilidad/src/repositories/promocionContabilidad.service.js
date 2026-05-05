2ac27f2427e1ccfe267fce09b4c6ce07894fb47b
ramosdeleon555 <124799275+ramosdeleon555@users.noreply.github.com>
2026-05-05 09:20:08 -0600

    Admin-conta Jeff. Daniel Ramos Create promocionContabilidad.service.js

---
+++ services/admin-contabilidad/src/repositories/promocionContabilidad.service.js
@@ -0,0 +1,24 @@
+//Admin-conta Jeff. Daniel Ramos
+const repo = require('../repositories/promocionContabilidad.repository');
+
+const registrarPromocionAplicada = async ({
+  pedido_id,
+  cliente_id,
+  promocion_id,
+  tipo_alcance,
+  referencia_id,
+  monto_descuento
+}) => {
+  return await repo.crearRegistroPromocion({
+    pedido_id,
+    cliente_id,
+    promocion_id,
+    tipo_alcance,
+    referencia_id,
+    monto_descuento
+  });
+};
+
+module.exports = {
+  registrarPromocionAplicada
+};
