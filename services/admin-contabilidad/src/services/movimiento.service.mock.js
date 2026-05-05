3da39cf03021f78803c5474f1249732e4114a957
ramosdeleon555 <124799275+ramosdeleon555@users.noreply.github.com>
2026-05-05 11:08:11 -0600

    Admin-conta Jeff. Daniel Ramos Create movimiento.service.mock.js

---
+++ services/admin-contabilidad/src/services/movimiento.service.mock.js
@@ -0,0 +1,10 @@
+//Admin-conta Jeff. Daniel Ramos
+module.exports = {
+  registrarEgreso: async (data) => {
+    console.log('MOCK EGRESO:', data);
+  },
+
+  registrarIngresoPedido: async (data) => {
+    console.log('MOCK INGRESO:', data);
+  }
+};
