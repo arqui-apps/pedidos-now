f9b298478b1e94d3ffc6b0d31603d7ce1b29caf3
ramosdeleon555 <124799275+ramosdeleon555@users.noreply.github.com>
2026-05-05 10:49:10 -0600

    Adding microservice Descuentos Jeff. Daniel Ramos Update index.js

--- services/admin-contabilidad/src/events/index.js
+++ services/admin-contabilidad/src/events/index.js
@@ -1,17 +1,30 @@
-// Admin-contabilidad Jeff
+// Admin-contabilidad Kenneth
+const paqueteriaHandler = require('./paqueteria/paqueteria.handler');
+// Admin-contabilidad Emmanuel
+const restaurantesHandler = require('./restaurantes/restaurantes.handler');
+// Admin-contabilidad Jeff. Daniel Ramos
 const descuentosHandler = require('./descuentos/descuentos.handler');
+
 
 exports.procesarEvento = async (evento) => {
   try {
     switch (evento.modulo) {
+      case 'paqueteria':
+        await paqueteriaHandler(evento);
+        break;
+
+      case 'restaurantes':
+        await restaurantesHandler(evento);
+        break;
+
       case 'descuentos':
         await descuentosHandler(evento);
         break;
 
       default:
-        console.log('Evento no manejado:', evento.tipo);
+        console.log(' Evento no manejado:', evento.tipo);
     }
   } catch (error) {
-    console.error('Error procesando evento:', error);
+    console.error(' Error procesando evento:', error);
   }
 };
